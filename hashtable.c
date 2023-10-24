b7de7b58adb50adeed59db2e47388f2a33376a06
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2023-10-24 01:33:49 +0000

    Inicializando RandomHashFunction asignando valores aleatorios a la tabla hash

--- hashtable.c
+++ hashtable.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 struct RandomHashFunction {
     int* hashTable;
 };
+
+void initializeRandomHasher(struct RandomHashFunction* hasher, int M, int n) {
+    hasher->hashTable = (int*)malloc(M * sizeof(int));
+    srand(time(NULL));
+
+    for (int i = 0; i < M; i++) {
+        hasher->hashTable[i] = rand() % n;
+    }
+}
